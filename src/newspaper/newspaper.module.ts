2bc64dbf91091506984ddd86a95f7724f9b021ab
Luca Terrazzan <luca.terrazzan@docebo.com>
2019-06-10 11:46:04 +0200

    Removed newspaper resource

--- src/newspaper/newspaper.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NewspaperController } from './newspaper.controller';
-import { NewspaperService } from './newspaper.service';
-
-@Module({
-  controllers: [NewspaperController],
-  providers: [NewspaperService]
-})
-export class NewspaperModule {}
