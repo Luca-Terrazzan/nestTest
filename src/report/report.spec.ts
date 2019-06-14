b211f90e02bebc74f68b0370343c0cabc26f8d4f
Luca Terrazzan <luca.terrazzan@docebo.com>
2019-06-14 12:37:11 +0200

    mess

--- src/report/report.spec.ts
+++ src/report/report.spec.ts
@@ -1,11 +1,12 @@
 import { Report } from './report';
 import { Users } from './reportTypes/users';
+import { Courses } from './reportTypes/courses';
 
 describe('Report', () => {
-  const report = new Report<Users>(new Users());
+  const report = new Report<Users, Courses>(new Users(), new Courses());
 
   it('Should have a report type name', () => {
-    expect(report.getReportTypeName()).toBeDefined();
+    expect(report.getReportMetadata()).toBeDefined();
   });
 
 });
--- src/report/report.ts
+++ src/report/report.ts
@@ -1,16 +1,18 @@
 import { IReportType } from './ireporttype';
 import { ReportMetadata } from './types/reportMetadata';
 
-export class Report<T extends IReportType> {
+export class Report<T extends IReportType, K extends IReportType> {
 
-  constructor(private readonly reportMetadata: T) { }
+  constructor(
+    private readonly firstEntityMetadata: T,
+    private readonly secondEntityMetadata: K
+  ) { }
 
-  public getReportTypeName(): string {
-    return this.reportMetadata.getTypeName();
-  }
-
-  public getReportMetadata(): ReportMetadata {
-    return this.reportMetadata.getReportTypeMetadata();
+  public getReportMetadata(): {firstEntityMetadata: ReportMetadata, secondEntityMetadata: ReportMetadata} {
+    return {
+      firstEntityMetadata: this.firstEntityMetadata.getReportTypeMetadata(),
+      secondEntityMetadata: this.secondEntityMetadata.getReportTypeMetadata()
+    };
   }
 
 }
