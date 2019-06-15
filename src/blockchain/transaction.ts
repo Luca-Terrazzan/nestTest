6d15ff551127b369c1639e36d5c8aca417e37559
Luca Terrazzan <luca.terrazzan@docebo.com>
2019-06-15 15:47:06 +0200

    added transaction class

---
+++ src/blockchain/transaction.ts
@@ -0,0 +1,10 @@
+import { User } from './chain-user';
+
+export class Transaction {
+  constructor(
+    private readonly giver: User,
+    private readonly receiver: User,
+    private readonly amount: number) {
+
+  }
+}
