e790f9c1fd70fe52b1bd57ac119be344289d5faa
Luca Terrazzan <luca.terrazzan@docebo.com>
2019-06-15 16:12:50 +0200

    block simple implementation

--- src/blockchain/block.ts
+++ src/blockchain/block.ts
@@ -1,5 +1,19 @@
-export class Block {
-  constructor(private readonly precedent: Block) {
+import { hashBlock } from './utils';
+import { HttpAdapterHost } from '@nestjs/core';
 
+export class Block {
+
+  private readonly hash: string;
+
+  constructor(private readonly previousBlockHash: string) {
+    this.hash = hashBlock(this);
   }
+
+  /**
+   * getHash
+   */
+  public getHash(): string {
+    return this.hash;
+  }
+
 }
