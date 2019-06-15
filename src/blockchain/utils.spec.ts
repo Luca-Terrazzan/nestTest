bf8dbb0d5c6f40f4171da2fb04fef258ecdad5be
Luca Terrazzan <luca.terrazzan@docebo.com>
2019-06-15 17:00:05 +0200

    better block testing

--- src/blockchain/utils.spec.ts
+++ src/blockchain/utils.spec.ts
@@ -1,15 +1,18 @@
-import { hashBlock } from './utils';
-import { Transaction } from './transaction';
-import { User } from './chain-user';
+import { Block } from './block';
 import { ChainAncestorBlock } from './chain-ancestor-block';
+import { User } from './chain-user';
+import { Transaction } from './transaction';
+import { hashBlock } from './utils';
 
 describe('Blockchain hash function', () => {
 
-  it('Should be able to hash a block', () => {
+  it('Should be able to hash any block', () => {
     const transaction = new Transaction(new User('a'), new User('b'), 1);
     const aBlock = new ChainAncestorBlock(transaction);
-
     expect(hashBlock(aBlock)).toBeDefined();
+
+    const bBlock = new Block(aBlock.getHash(), transaction);
+    expect(hashBlock(bBlock)).toBeDefined();
   });
 
 });
